6ce891356ac68492f219b9610d6dd55148973fb0
Maki Ikegami <ikegami@fork.co.jp>
2020-01-15 19:23:56 +0900

    難易度・モード選択を追加

--- src/pages/battle/index.js
+++ src/pages/battle/index.js
@@ -1,99 +1,206 @@
 export default {
   data() {
     return {
-      stationAll: [],
-      stationRemaining: [],
-      stationExpress: ['中央林間', '南町田グランベリーパーク', '長津田'],
-      stationLocal: ['つきみ野', 'すずかけ台', 'つくし野'],
+      // 駅名系まとめ
+      sta: {
+        all: [],
+        remaining: [], // 残選択肢
+        express: ['中央林間', '南町田グランベリーパーク', '長津田'],
+        local: ['つきみ野', 'すずかけ台', 'つくし野'],
+        cpu: [], // cpu知識
+      },
+
+      // フェーズ
+      phase: 0,
+
+      // 設定
+      config: {
+        difficulty: 'normal', // normal,
+        mode: 'all', // all,
+      },
 
       myAnswer: '',
-
-      CPUKnowledge: [],
 
       answerHistory: [],
 
       msg: '',
 
       // オーライボタン発車・入力可能
-      isEnabledSubmit: false,
-      isEnabledInput: false,
+      isEnabled: {
+        submit: false,
+        input: false
+      },
+
+      // タイムアタック
+      time: {
+        start: '',
+        end: '',
+        diff: '',
+      }
     }
   },
-  created() {
-    // ゲームに使われる全駅の配列を作成しておく
-    this.stationAll = this.stationExpress.concat(this.stationLocal);
-
-    // こちらは解答できる残選択肢の駅名配列
-    this.stationRemaining = this.stationAll;
-
-    // 今回のCPUが知っている駅名をランダム生成
-    this.CPUKnowledge = this.makeCPUKnowledge(this.stationAll, this.stationAll.length / 2);
-  },
   computed: {
-    // 解答入力されているかチェック
-    activateSubmit() {
-      let state;
-      if(this.myAnswer.length > 0) {
-        state = true;
-      } else {
-        state = false;
+    // 解答を入力されたら
+    inputMyAnswer: {
+      get () {
+        return this.myAnswer;
+      },
+      set(value) {
+        this.myAnswer = value;
+        if(value.length < 1) {
+          this.isEnabled.submit = false;
+        } else {
+          this.isEnabled.submit = true;
+        }
       }
-      this.isEnabledSubmit = state;
-    },
+    }
   },
   methods: {
-    // cpuの選択肢を生成
+    /**
+     * ゲーム開始時
+     */
+    startGame() {
+      // 設定に基づき選択肢を生成
+      switch(this.config.mode) {
+        // モード: すべての駅
+        case 'all':
+          this.sta.all = this.sta.express.concat(this.sta.local);
+          break;
+        // モード: 急行
+        case 'express':
+          this.sta.all = this.sta.express;
+          break;
+        // モード: 各駅
+        case 'local':
+          this.sta.all = this.sta.local;
+          break;
+        // 念の為デフォルトはすべての駅とする
+        default:
+          this.sta.all = this.sta.express.concat(this.sta.local);
+      }
+
+      // 残選択肢
+      this.sta.remaining = this.sta.all;
+
+      // cpu知識
+      let numCpuKnowledge = 0;
+      switch(this.config.difficulty) {
+        // 難易度: ふつう
+        case 'normal':
+          numCpuKnowledge = Math.round(this.sta.all.length / 2);
+          this.sta.cpu = this.makeCPUKnowledge(this.sta.all, numCpuKnowledge);
+          break;
+        // 難易度: 神
+        case 'god':
+          this.sta.cpu = this.sta.all;
+          break;
+        // 念の為デフォルトはふつうとする
+        default:
+          numCpuKnowledge = Math.round(this.sta.all.length / 2);
+          this.sta.cpu = this.makeCPUKnowledge(this.sta.all, numCpuKnowledge);
+      }
+
+      // フェーズ進行
+      this.phase = 1;
+      // 開始時間保存
+      this.time.start = new Date();
+      // 入力可能にする
+      this.isEnabled.input = true;
+    },
+
+    /**
+     * cpuの知識を生成
+     * @param {array} array 全駅のデータ
+     * @param {number} count 知識の数
+     * @return {array} cpuの知識を配列で返す
+     */
     makeCPUKnowledge(array, count) {
-      const arr = array;
-      const num = count;
       const result = [];
-      for(let i = 0; i < num; i++) {
-        const randomIndex = Math.floor(Math.random() * ((arr.length - 1)));
-        if(result.some(item => item === arr[randomIndex])) {
+      for(let i = 0; i < count; i++) {
+        const randomIndex = Math.floor(Math.random() * ((array.length - 1)));
+        if(result.some(item => item === array[randomIndex])) {
           i--;
         } else {
-          result[i] = arr[randomIndex];
+          result[i] = array[randomIndex];
         }
       }
       return result;
     },
 
-    // オーライしたとき
+    /**
+     * オーライ押下時
+     */
     submitMyAnswer() {
       // 入力された答え
       const answer = this.myAnswer;
       // 残選択肢に存在するかチェック
-      const isCorrect = this.stationRemaining.some(item => item === answer);
+      const isCorrect = this.sta.remaining.some(item => item === answer);
       // 全選択肢にはあったかチェック（おしい）
-      const isClose = this.stationAll.some(item => item === answer);
-      let msg;
+      const isClose = this.sta.all.some(item => item === answer);
 
       // 入力エリアを空にしておく
       this.myAnswer = '';
 
       if(isCorrect) { // 残選択肢にあれば
-        // 解答履歴配列に追加
-        this.answerHistory.push({'human': true, 'content': answer});
-        // 解答された選択肢を消す
-        this.stationRemaining = this.stationRemaining.filter(item => item !== answer);
-        this.CPUKnowledge = this.CPUKnowledge.filter(item => item !== answer);
-        // 相手のターン
+        // 解答処理
+        this.processWhenAnswer(true, answer);
+
+        // 相手のターンへ
         this.processCPUTurn();
       } else if(isClose) { // 残選択肢にはないが全選択肢にはあれば
-        msg = answer + 'はもうでたよ';
+        this.msg = answer + 'はもうでたよ';
       } else { // 残選択肢になければ
-        msg = answer + 'はないよ';
+        this.msg = answer + 'はちがうよ';
       }
-
-      // メッセージ表示
-      this.msg = msg;
     },
 
-    // 相手のターン
+    /**
+     * 解答成功時
+     * @param {boolean} human 人間の解答ならばtrue
+     * @param {string} ans 解答駅名
+     */
+    processWhenAnswer(human, ans) {
+      // 解答履歴配列に追加
+      this.answerHistory.push({human, 'content': ans});
+      // 解答された選択肢を消す
+      this.sta.remaining = this.sta.remaining.filter(item => item !== ans);
+      this.sta.cpu = this.sta.cpu.filter(item => item !== ans);
+    },
+
+    /**
+     * cpuのターン
+     */
     processCPUTurn() {
       // 入力・送信禁止
-      this.isEnabledInput = false;
-      this.isEnabledSubmit = false;
-    }
+      this.isEnabled.input = false;
+      this.isEnabled.submit = false;
+
+      // 残選択肢がなければ投了
+      if(this.sta.cpu.length < 1) {
+        this.msg = 'CPU：負けました・・・';
+
+        // ゲーム終了処理へ
+        this.endGame();
+      } else {
+        // あればランダムで選んで解答
+        const choice = this.sta.cpu[Math.floor(Math.random() * (this.sta.cpu.length))];
+        // 解答処理
+        this.processWhenAnswer(false, choice);
+
+        // 入力許可
+        this.isEnabled.input = true;
+        this.isEnabled.submit = true;
+      }
+    },
+
+    /**
+     * ゲーム終了時
+     */
+    endGame() {
+      // 終了時間保存
+      this.time.end = new Date();
+      // 差分（クリア時間）
+      this.time.diff = (this.time.end.getTime() - this.time.start.getTime()) / 1000;
+    },
   }
 }
--- src/pages/battle/index.vue
+++ src/pages/battle/index.vue
@@ -4,30 +4,64 @@
 
 <template lang="pug">
 .battle
-  // 設定
-  p 難易度
-  div
-    button normal
-  button wish you the best
+  // フェーズ０
+  .config(v-if="phase === 0")
+    // 設定
+    dl
+      dt.config-term 難易度を選んでね
+      dd
+        ul.config-list
+          li.config-item
+            label.radio
+              input(type="radio" value="normal" v-model="config.difficulty")
+              span.radio-tx ふつう
+          li.config-item
+            label.radio
+              input(type="radio" value="god" v-model="config.difficulty" class="radio")
+              span.radio-tx 神
 
-  // 臨時残選択肢エリア
-  p 全部の選択肢: {{ stationAll }}
-  p 残りの選択肢: {{ stationRemaining }}
-  p CPU残選択肢: {{ CPUKnowledge }}
+      dt.config-term モードを選んでね
+      dd
+        ul.config-list
+          li.config-item
+            label.radio
+              input(type="radio" value="all" v-model="config.mode")
+              span.radio-tx すべての駅
+          li.config-item
+            label.radio
+              input(type="radio" value="express" v-model="config.mode")
+              span.radio-tx 急行停車駅のみ
+          li.config-item
+            label.radio
+              input(type="radio" value="local" v-model="config.mode")
+              span.radio-tx 急行に飛ばされる駅のみ
+    button(@click="startGame()" type="button" class="config-btn") はじめる
 
-  // 対戦エリア
-  ul
-    li(v-for="answer in answerHistory")
-      p
-        span(v-if="answer.human") あなた
-        span(v-else) あいて
-        |：{{ answer.content }}
-  p {{ msg }}
+  // フェーズ１
+  template(v-if="phase === 1")
+    // 臨時残選択肢エリア
+    p 全部の選択肢: {{ sta.all }}
+    p 残りの選択肢: {{ sta.remaining }}
+    p CPU残選択肢: {{ sta.cpu }}
+
+    // 対戦エリア
+    ol
+      li(v-for="answer in answerHistory")
+        p
+          span(v-if="answer.human") あなた
+          span(v-else) CPU
+          |：{{ answer.content }}
+    p {{ msg }}
+
+    // 時間
+    p 開始： {{ time.start }}
+    p 終了： {{ time.end }}
+    p クリアタイム： {{ time.diff }}秒
 
   // 入力欄
   form.answer(@submit.prevent="submitMyAnswer")
-    input(type="text" v-model="myAnswer").answer-input
-    button.answer-submit(v-bind:disabled="!isEnabledSubmit") オーライ
+    input(type="text" v-model="inputMyAnswer" v-bind:disabled="!isEnabled.input" placeholder="駅名を入力してね").answer-input
+    button.answer-submit(v-bind:disabled="!isEnabled.submit") オーライ
 </template>
 
 <script src="./index.js"></script>
